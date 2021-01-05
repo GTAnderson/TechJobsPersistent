33d23c1c526005331d6d19f03d0076489d1e68a0
GTAnderson <garrett.t.anderson@gmail.com>
2021-01-05 00:05:12 -0600

    Part 2, Adding a Job, Work In Progress on Step 3.

---
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TechJobsPersistent.Models;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddJobViewModel
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public List<SelectListItem> Employers { get; set; }
+
+        public AddJobViewModel(string name, int id, List<SelectListItem> Employers)
+        {
+            Name = name;
+            Id = id;
+            Employers = new List<SelectListItem>();
+        }
+
+        public AddJobViewModel()
+        {
+        }
+    }
+}
